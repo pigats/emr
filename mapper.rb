a4c20fdd628df88054bde41b80f7538e971d9c89
pigats <andrea@fruktarbo.com>
2012-10-23 17:11:57 +0200

    fix mapper.rb w/ ruby env

--- mapper.rb
+++ mapper.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # encoding: UTF-8
 
 require 'json'
@@ -5,8 +6,13 @@ require 'json'
 STDIN.each do |line|
   begin
     tweet = JSON.parse line
-    tweet['entities']['hashtags'].each do { |hashtag| puts "LongValueSum:#{hashtag}\t1" }
-  rescue Exception => e 
+    tweet['entities']['hashtags'].each do |hashtag| 
+      hashtag = hashtag['text'].downcase.strip      
+      hashtag = hashtag[0...(hashtag.length*0.8).to_f.floor] if hashtag.length > 3 # stemming-of-the-poors
+      puts "LongValueSum:#{hashtag}\t1" 
+    end
+
+  rescue Exception => e
   end
 
 end
