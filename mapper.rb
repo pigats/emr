7954c7ee96cd79b5975dd51dd88ad5ca1c20f461
Edoardo Tenani <edoardo.tenani@gmail.com>
2012-10-24 12:30:49 +0200

    removed stemming from mapper.rb

--- mapper.rb
+++ mapper.rb
@@ -8,7 +8,6 @@ STDIN.each do |line|
     tweet = JSON.parse line
     tweet['entities']['hashtags'].each do |hashtag| 
       hashtag = hashtag['text'].downcase.strip      
-      hashtag = hashtag[0...(hashtag.length*0.8).to_f.floor] if hashtag.length > 3 # stemming-of-the-poors
       puts "LongValueSum:#{hashtag}\t1" 
     end
 
