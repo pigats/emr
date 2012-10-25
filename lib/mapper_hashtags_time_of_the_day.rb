90963c0591feff7a8722ae178b32cac4a7e5b390
pigats <andrea@fruktarbo.com>
2012-10-25 10:41:10 +0200

    clean working directory

---
+++ lib/mapper_hashtags_time_of_the_day.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+require 'json'
+require 'time'
+
+STDIN.each do |line|
+  begin
+    tweet = JSON.parse line
+    normalized_hour = Time.at(Time.utc(tweet['created_at']).to_i - tweet['user']['utc_offset']).utc.hour
+    tweet['entities']['hashtags'].each do |hashtag| 
+      hashtag = hashtag['text'].downcase.strip      
+      puts "#{hashtag}\t#{normalized_hour}"
+
+    end
+
+  rescue Exception => e
+  end
+
+end
